7c0fadb788d6073f04a1c3e2efe2b7ba9b1b1a8e
darkend <hiby.90hou@qq.com>
2018-11-05 10:01:40 +1100

    reset database

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_11_02_113922) do
+ActiveRecord::Schema.define(version: 2018_11_01_123704) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -21,10 +21,10 @@ ActiveRecord::Schema.define(version: 2018_11_02_113922) do
     t.string "last_name"
     t.date "date_of_birth"
     t.string "email"
-    t.string "user_id"
-    t.string "references"
+    t.bigint "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_customers_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
