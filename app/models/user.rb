f4e5ab8c37c7fa7ee687d0146201de1e92df2d94
darkend <hiby.90hou@qq.com>
2018-11-04 22:28:54 +1100

    customer email must be unique

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,8 +3,8 @@ class User < ApplicationRecord
 	VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
 	
 	#Validations
-  validates_presence_of :name, :email, :password_digest
-  validates :email, format: { with: VALID_EMAIL_REGEX }, uniqueness: { case_sensitive: false }
+  validates_presence_of :name, :password_digest
+  validates :email, presence: true, format: { with: VALID_EMAIL_REGEX }, uniqueness: { case_sensitive: false }
  
   #encrypt password
   has_secure_password
